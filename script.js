0dfdc95f119abb5a48344364f7689e1172c58d84
omkarshinde14 <114727827+omkarshinde14@users.noreply.github.com>
2024-03-21 01:07:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
